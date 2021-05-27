61c004d1bad278a7e55b12bd23bf79fa5c0041a2
mohabelhbashy <mohab.hafez332@gmail.com>
2021-05-27 23:52:09 +0200

    Design

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Regest from './Components/Regestration/Regest'
+function App() {
+  return (
+    <div className="My-App">
+     <Regest/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Regestration/Regest.js
@@ -0,0 +1,41 @@
+import React,{useState} from 'react'
+import Style from './Regest.module.css';
+import { BsChatDotsFill } from 'react-icons/bs';
+
+
+function Regest() {
+    const [CheckLogin,setCheckLogin]=useState(true)
+    return (
+        <div className={Style.RegestContent}>
+               <div>
+               <BsChatDotsFill size="100" color="#ff7f00"/> 
+               <h2 style={{fontWeight:'bold',fontStyle:'italic',color:'white'}}>My Chat . . . </h2>
+<h4 style={{fontWeight:'bold',fontStyle:'italic',color:'white'}}>Chat To every one and make more friends</h4>    
+<div style={{color:'white'}}>
+   <div className={Style.DivLogin}>
+       <h2 onClick={()=>setCheckLogin(true)} style={{color:CheckLogin?'orange':'white'}}>Log in</h2> <h2>-</h2>
+       <h2 onClick={()=>setCheckLogin(false)} style={{color:!CheckLogin?'orange':'white'}}>Sign up</h2></div> 
+           <div className={Style.DivInput} style={{display:CheckLogin?"flex":"none"}} >
+                      <input placeholder="User name" type='username'/>
+                      <input placeholder='password' type='password'/>
+                      <button className={Style.Buttons}>Log In</button>
+
+                  </div>
+                  <div className={Style.DivInputSignup} style={{display:!CheckLogin?"grid":"none"}} >
+                      <input placeholder="First name" type='text'/>
+                      <input placeholder="Last name" type='text'/>
+                      <input placeholder="Phone number" type='text'/>
+                      <input placeholder="email" type='text'/>
+                      <input className={Style.III} placeholder='password' type='password'/><br/>
+                      <button className={Style.Buttons}>Sign up</button>
+
+                  </div>
+           </div> 
+            </div> 
+         
+           
+                   </div>
+    )
+}
+
+export default Regest
