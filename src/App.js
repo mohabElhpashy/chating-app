e5f70cf08c25b42a2d0fc907cd5a1bb041e7030d
mohabelhbashy <mohab.hafez332@gmail.com>
2021-05-28 19:41:09 +0200

    All done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Regest from './Components/Regestration/Regest'
 import Chatting from './Components/Chatting/Chatting'
 function App() {
   return (
     <div className="My-App">
-     {/* <Regest/> */}
      <Chatting/>
     </div>
   );
